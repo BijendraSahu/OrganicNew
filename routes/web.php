94d48eb1f10a973a9d78144bca2b6e944bd21041
Aditya Shrivastava <aadishriz@gmail.com>
2018-08-23 12:07:12 +0530

    Merge branch 'Bijendra' of https://github.com/BijendraSahu/OrganicNew into aditya
    # Conflicts:
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -173,6 +173,4 @@ Route::get('/addblogcat','BlogController@addblogcat');
 Route::get('/blogpost','BlogController@blogpost');
 Route::post('/blogpic','BlogController@blogpic');
 //////////////////////////////////////////////////////////////////////////////////////////////
-//cxzczxczxcxzczxcxzcdsfafdsagadfxzcxczavdzdfasgfdasgdfa
-//dsffdsfdsfsdfds
-//fdsgdasf
+//public function
